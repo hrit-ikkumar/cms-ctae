5ccdd6ecd702d88a858fecba71a573ae804fe414
Hritik Kumar Sharma <hritik@myanatomy.in>
2021-06-30 01:54:54 +0530

    Updated production build

--- cms-ctae-production/routes/Club/club.js
+++ cms-ctae-production/routes/Club/club.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const Club = require("../../Model/Club"); // mongoose.model('User');
+const User = require("../../Model/User");
+
+const { ClubWiseUser } = User;
 
 // All Club Details
 router.post("/getClubData", (req, res, next) => {
@@ -9,10 +12,30 @@ router.post("/getClubData", (req, res, next) => {
   Club.findOne({ clubName: clubName })
     .then((result) => {
       if (result != null) {
-        res.statusCode = 200;
-
-        res.send(result);
-        return;
+        ClubWiseUser(result.clubCode)
+          .find({})
+          .then((members) => {
+            res.statusCode = 200;
+            result.clubMembers = members.map((member) => {
+              return {
+                name: member.name,
+                email: member.email,
+                clubPosition: member.clubPosition,
+                avatar:
+                  "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+              };
+            });
+            res.send(result);
+            console.log(result);
+            return;
+          })
+          .catch((err) => {
+            if (err !== null) {
+              res.statusCode = 406;
+              res.send();
+              return;
+            }
+          });
       } else {
         res.statusCode = 406;
         res.send({ err: "Don't have any records in our DB" });
@@ -32,6 +55,7 @@ router.post("/register", (req, res, next) => {
     clubName,
     clubCode,
     clubLogo,
+    clubBanner,
     clubPhotos,
     clubObjectives,
     socialMedia,
@@ -41,6 +65,7 @@ router.post("/register", (req, res, next) => {
     clubName: clubName,
     clubCode: clubCode,
     clubLogo: clubLogo,
+    clubBanner: clubBanner,
     clubPhotos: clubPhotos,
     clubObjectives: clubObjectives,
     socialMedia: socialMedia,
@@ -57,6 +82,64 @@ router.post("/register", (req, res, next) => {
           .then((result) => {
             res.statusCode = 200;
             res.send(result);
+            return;
+          })
+          .catch((err) => {
+            if (err != null) {
+              res.statusCode = 500;
+              res.send();
+              return;
+            }
+          });
+      }
+    })
+    .catch((err) => {
+      if (err != null) {
+        res.statusCode = 406;
+        res.send();
+        return;
+      }
+    });
+});
+
+// Update a Club in System
+router.put("/update", (req, res, next) => {
+  const {
+    clubName,
+    clubCode,
+    clubLogo,
+    clubPhotos,
+    clubBanner,
+    clubObjectives,
+    socialMedia,
+    prevData,
+  } = req.body;
+  const newClub = {
+    clubName: clubName,
+    clubCode: clubCode,
+    clubLogo: clubLogo,
+    clubBanner: clubBanner,
+    clubPhotos: clubPhotos,
+    clubObjectives: clubObjectives,
+    socialMedia: socialMedia,
+  };
+  Club.findOne({ clubName: prevData.clubName })
+    .then((result) => {
+      if (result === null) {
+        throw error;
+      } else {
+        Club.findOneAndUpdate(
+          {
+            _id: prevData._id,
+            clubName: prevData.clubName,
+            clubCode: prevData.clubCode,
+          },
+          { $set: newClub },
+          { useFindAndModify: true }
+        )
+          .then((result) => {
+            res.statusCode = 200;
+            res.send({ ...prevData, ...newClub });
             return;
           })
           .catch((err) => {
