51f2dc5bf35cfd23d248d7c6746be0bce87deb8d
Hritik Kumar Sharma <hritik@myanatomy.in>
2021-07-09 00:41:51 +0530

    Added display image router endpoint in backend

--- cms-ctae-backend/routes/Image/image.js
+++ cms-ctae-backend/routes/Image/image.js
@@ -75,4 +75,26 @@ router.get("/:filename", (req, res) => {
   });
 });
 
+router.get("/view/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exists",
+      });
+    }
+
+    // Check if image
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: "Not an image",
+      });
+    }
+  });
+});
+
 module.exports = router;
