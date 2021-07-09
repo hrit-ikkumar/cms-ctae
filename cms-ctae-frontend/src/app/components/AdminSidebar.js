839dff0b09d94c895b3c5b798b009b9496ce1499
Hritik Kumar Sharma <hritik@myanatomy.in>
2021-07-09 14:15:26 +0530

    updated frontend code for local use

--- cms-ctae-frontend/src/app/components/AdminSidebar.js
+++ cms-ctae-frontend/src/app/components/AdminSidebar.js
@@ -19,7 +19,7 @@ function AdminSidebar() {
       <SidebarHeader>
         <ClubIcon
           loading="eager"
-          src={`https://cms-ctae.herokuapp.com/upload/images/view/${clubInfo.clubLogo}`}
+          src={`http://localhost:3000/upload/images/view/${clubInfo.clubLogo}`}
         />
         <HeaderTitle>{clubInfo.clubName}</HeaderTitle>
       </SidebarHeader>
